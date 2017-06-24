bf14a3b1fcfafaf00c6be0acb7d6f04091e6b029
Marco Segreto <msegreto@miceover.com>
2017-06-24 17:09:32 +0200

    Got walls to mosly render
    Top wall not appearing for some reason not sure why. Collision not
    working on walls yet.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
+#include <vector>
 
 #include <Box2D/Box2D.h>
 #include <SFML/Graphics.hpp>
 
 const float DRAW_FACTOR = 10.0f;
+const float width = 60.0f;
+const float height = 36.0f;
+const float PLAYER_R = 0.35;
+const float BALL_R = 0.23;
 
 b2Vec2 transformForDrawing(const b2Vec2& val)
 {
@@ -23,9 +28,43 @@ int main()
 
   float pw = 5.0f;
   float ph = 5.0f;
+  b2Vec2 walls[16];
+  // Extra padding added to walls so they're full on screen
+  const float w = 40.0f;
+  const float h = 24.0f;
+  walls[0].Set(-width * .5 +0.2 +w, -height * .5 +h);
+  walls[1].Set(-width * .5 +w, -height * .5 +0.2 +h);
+  walls[2].Set(-width * .5 +w, -height * .2 +h);
+  walls[3].Set(-width * .6 +w, -height * .2 +h);
+  walls[4].Set(-width * .6 +w, +height * .2 +h);
+  walls[5].Set(-width * .5 +w, +height * .2 +h);
+  walls[6].Set(-width * .5 +w, +height * .5 -.2 +h);
+  walls[7].Set(-width * .5 +.2 +w, +height * .5 +h);
+  walls[8].Set(+width * .5 -.2 +w, +height * .5 +h);
+  walls[9].Set(+width * .5 +w, +height * .5 -.2 +h);
+  walls[10].Set(+width * .5 +w, +height * .2 +h);
+  walls[11].Set(+width * .6 +w, +height * .2 +h);
+  walls[12].Set(+width * .6 +w, -height * .2 +h);
+  walls[13].Set(+width * .5 +w, -height * .2 +h);
+  walls[14].Set(+width * .5 +w, -height * .5 +.2 +h);
+  walls[15].Set(+width * .5 -.2 +w, -height * .5 +h);
+
+  b2ChainShape wallChain;
+  wallChain.CreateChain(walls, 16);
+
+  b2FixtureDef wallFixDef;
+  wallFixDef.shape = &wallChain;
+  wallFixDef.friction = 0;
+  wallFixDef.restitution = 0;
+
+  b2BodyDef wallDef;
+  wallDef.type = b2_dynamicBody;
+  b2Body* wall = world.CreateBody(&wallDef);
+  wall->CreateFixture(&wallFixDef);
+
   b2BodyDef playerDef;
   playerDef.type = b2_dynamicBody;
-  playerDef.position.Set(10.0f, 10.0f);
+  playerDef.position.Set(50.0f, 10.0f);
   b2Body* player = world.CreateBody(&playerDef);
   b2PolygonShape playerBox;
   playerBox.SetAsBox(pw, ph);
@@ -34,10 +73,21 @@ int main()
   fixtureDef.density = 1.0f;
   fixtureDef.friction = 0.3f;
   player->CreateFixture(&fixtureDef);
-  b2Vec2 force(5000.0f, 1000.0f);
+  b2Vec2 force(-5000.0f, 1000.0f);
   player->ApplyForceToCenter(force, true);
 
-  sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+  sf::RenderWindow window(sf::VideoMode(1250, 800), "SFML works!");
+
+  sf::VertexArray wallLines(sf::LinesStrip, 16);
+  for( unsigned int i = 0; i < sizeof(walls)/sizeof(walls[0]); i = i + 1 )
+  {
+    b2Vec2 transformed = transformForDrawing(walls[i]);
+    wallLines[i].position = sf::Vector2f(transformed.x, transformed.y);
+    wallLines[i].color = sf::Color::Red;
+    printf("%4.2f %4.2f \n", transformed.x, transformed.y);
+  }
+
+  printf("%4.2f %4.2f %4.2f %4.2f \n", wallLines.getBounds().left, wallLines.getBounds().top, wallLines.getBounds().width, wallLines.getBounds().height);
 
   sf::RectangleShape playerR;
   playerR.setSize(sf::Vector2f(pw * DRAW_FACTOR, ph * DRAW_FACTOR));
@@ -56,7 +106,7 @@ int main()
     world.Step(timeStep, velocityIterations, positionIterations);
     b2Vec2 pPos = player->GetPosition();
     float32 pAngle = player->GetAngle();
-    printf("%4.2f %4.2f %4.2f\n", pPos.x, pPos.y, pAngle);
+    //printf("%4.2f %4.2f %4.2f\n", pPos.x, pPos.y, pAngle);
 
     b2Vec2 transformedPos = transformForDrawing(pPos);
     playerR.setPosition(transformedPos.x, transformedPos.y);
@@ -64,6 +114,7 @@ int main()
 
     window.clear();
     window.draw(playerR);
+    window.draw(wallLines);
     window.display();
   }
 
